9400ffa93fbf47834822d759b0e649f999c0571f
Vincent Spinosa <92981607+vincentspinosa@users.noreply.github.com>
2025-06-21 21:38:44 +0200

    Base foundations for all commands

---
+++ cmd_irc/src/NICK.cpp
@@ -0,0 +1,17 @@
+void	NICK()
+{
+	// si le Client n'est pas connecté, erreur
+
+	// si le nickname est vide, erreur
+
+	// si mauvaise syntaxe du nickname, erreur
+	// (erreur syntaxe : commence avec un préfixe de Channel, char invalide dans le nickname)
+
+	// si le nickname est déjà pris, erreur
+
+	// on enregistre l'ancien nickname dans une string
+
+	// on change le nickname
+
+	// on affiche que ancien nickname s'appelle maintenant nouveau nickname
+}
