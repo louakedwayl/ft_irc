74d77c5d4b8eb988483afc24591697335b6be814
Vincent Spinosa <92981607+vincentspinosa@users.noreply.github.com>
2025-06-23 11:21:19 +0000

    working on TOPIC.cpp

--- cmd_irc/src/TOPIC.cpp
+++ cmd_irc/src/TOPIC.cpp
@@ -1,18 +1,58 @@
-void	TOPIC()
+bool	valid_topic_request(std::string channel)
 {
-	// si extraction de l'input vers stringstream échoue, ou que channel name ou topic est vide -> erreur
+	if (/* Channel doesn't exist */)
+	{
+		// write error to Client
+		return (false);
+	}
 
-	// check if Channel exists, si non -> erreur
+	if (/* User is not in Channel */)
+	{
+		// write error to Client
+		return (false);
+	}
 
-	// check if user is in channel, si non -> erreur
+	if (/* Channel is restricted to operators AND User is not an operator */)
+	{
+		// write error to Client
+		return (false);
+	}
 
-	// si Topic est restreint aux operators et que le Client n'en est pas un -> error
+	return (true);
+}
 
-	// si il n'y a pas d'arguments -> Client veut seulement voir le Topic
-		
-		// si le Topic est vide, écrire cela
+void	TOPIC(std::string arg)
+{
+	std::stringstream 	ss(arg);
+	std::string 		channel;
+	std::string			topic;
 
-		// sinon, écrire le Topic
-	
-	// sinon -> changer le Topic de la Channel et l'écrire à tous ses membres.
+	if (!(ss >> channel >> topic) || channel.empty())
+		// write error to client
+
+	if (valid_topic_request(channel) == false)
+		return ;
+
+	Channel channel = ; // GET CHANNEL
+	User & 	user = ; // GET USER
+
+	if (topic.empty() == true) // pas d'args -> user veut seulement voir le Topic
+	{
+		if (channel.topic.empty() == true) // si il n'y a pas de Topic
+			// write to client that there is not Topic
+		else
+			// write the Topic to the Client
+	}
+	else // sinon, on change le Topic de la Channel et on l'écrit aux membres de la Channel
+	{
+		channel.set_channel_topic(topic);
+
+		std::set<int>::iterator it = channel.clients_fd.begin();
+		while (it != channel.clients_fd.end())
+		{
+			Client current_client = ; // GET CLIENT
+			// write the new Topic to the Client
+			it++;
+		}
+	}
 }
