1c809cc00622befd915cc8dfde1b17ed0ac40424
Vincent Spinosa <92981607+vincentspinosa@users.noreply.github.com>
2025-06-23 13:10:51 +0000

    working on PASS.cpp

--- cmd_irc/src/PASS.cpp
+++ cmd_irc/src/PASS.cpp
@@ -1,4 +1,4 @@
-void	PASS()
+void	PASS(std::string arg)
 {
 	// si pas d'utilisateur connecté, erreur
 	
@@ -7,4 +7,18 @@ void	PASS()
 	// check si mot de passe est pas vide, erreur si oui
 
 	// on change le mot de passe du Client
+
+	User & 		user = ; // GET USER
+	std::string server_password = arg;
+
+	if (/* User est déja connecté au server*/)
+		// écrire erreur à utilisateur
+	else if (/* Wrong password */)
+	{
+		// écrire ERR_PASSWDMISMATCH (464) au Client
+		// déconnecter le client
+	}
+	else
+		// set le password du Client
+
 }
