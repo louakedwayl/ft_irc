9eee1acb205661b9e4bd0dfcf365c0bdf775b8d3
Vincent Spinosa <92981607+vincentspinosa@users.noreply.github.com>
2025-06-23 13:13:03 +0000

    working on PASS.cpp

--- cmd_irc/src/PASS.cpp
+++ cmd_irc/src/PASS.cpp
@@ -1,13 +1,5 @@
 void	PASS(std::string arg)
 {
-	// si pas d'utilisateur connecté, erreur
-	
-	// si extraction du mot de passe vers stringstream échoue -> erreur
-
-	// check si mot de passe est pas vide, erreur si oui
-
-	// on change le mot de passe du Client
-
 	User & 		user = ; // GET USER
 	std::string server_password = arg;
 
